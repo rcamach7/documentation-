e35caca9e34d9da56256406034dcc7b06de7db4c
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-01-06 14:05:46 -0800

    add sections to website to expand on later

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import "./App.css";
+import React from "react";
+import DisplayData from "./components/DisplayData";
 
-function App() {
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <WebsiteTitle />
+        <AddNewResource />
+        <DisplayData />
+      </div>
+    );
+  }
+}
+
+function WebsiteTitle() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="App-title">
+      <h1>Documentations & Resources</h1>
+    </div>
+  );
+}
+
+function AddNewResource() {
+  return (
+    <div className="AddNewResource">
+      <button onClick={() => alert("Hello World")}>Add New Resource</button>
     </div>
   );
 }
---
+++ src/components/DisplayData.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class DisplayData extends React.Component {
+  render() {
+    return (
+      <div className="DisplayData">
+        <p>data</p>
+        <p>data</p>
+        <p>data</p>
+        <p>data</p>
+      </div>
+    );
+  }
+}
