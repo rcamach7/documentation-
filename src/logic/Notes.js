5c3d3764d3866d6072152495e26b446f9d9bb168
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-01-06 22:30:36 -0800

    cleaner form presentation and overall site improvement

--- src/logic/Notes.js
+++ src/logic/Notes.js
@@ -1,8 +1,9 @@
-const Note = (description, source) => {
+const Note = (title, description, source) => {
   const toString = () => {
     return `Description: ${description}. You can access this resource at ${source}`;
   };
   return {
+    title,
     description,
     source,
     toString,
